19a4450e25d38a4d0e4f9e610848fc6d9005e514
sawcon <gabbnicoh@gmail.com>
2022-11-06 18:16:34 +0800

    updated piglatin

--- Ch6/pitlatin.py
+++ Ch6/pitlatin.py
@@ -1,19 +1,52 @@
 # if a word starts with a vowel the word yay is added
-# if it begins with a consonant the consonant is moved at 
-# the end followed by ay
+# else the consonant is moved at the end followed by ay
 
-# separate non-letters at start of this word
+print('Enter the English Message')
+message = input()
+VOWELS = ('a', 'e', 'i', 'o', 'u', 'y')
 
-# separate non-letters at end of this word
+pigLatin = []
+for word in message.split():
+    # separate non-letters at start of this word
+    prefixNonLetters = ''
+    while len(word) > 0 and not word[0].isalpha():
+        prefixNonLetters += word[0]
+        word = word[1:]
+    if len(word) == 0:
+        pigLatin.append(prefixNonLetters)
+        continue
 
-# Remember if the word was in uppercase or titlecase
+    # separate non-letters at end of this word
+    suffixNonLetters = ''
+    while not word[-1].isalpha():
+        suffixNonLetters += word[-1]
+        word = word[:-1]
+    
+    # remember if the word was in uppercase or titlecase
+    wasUpper = word.isupper()
+    wasTitle = word.istitle()
+    word = word.lower()
 
-# Separate consonants at start of this word
+    # separate consonants at start of this word
+    prefixConsonants = ''
+    while len(word) > 0 and not word[0] in VOWELS:
+        prefixConsonants += word[0]
+        word = word[1:]
 
-# add the piglatin ending to the word:
-
-# set the word back to uppercase or title case
-
-# Add the non-letters back to the sart or end of the word.
+    # add the piglatin ending to the word:
+    if prefixConsonants != '':
+        word += prefixConsonants + 'ay'
+    else:
+        word += 'yay'
+    
+    # set the word back to uppercase or title case
+    if wasUpper:
+        word = word.upper()
+    if wasTitle:
+        word = word.title()
+    
+    # add the non-letters back to the sart or end of the word.
+    pigLatin.append(prefixNonLetters + word + suffixNonLetters)
 
 # join all the words back together in a single string.
+print(' '.join(pigLatin))
